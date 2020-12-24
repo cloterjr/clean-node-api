d353f6898728fe4b0aaf4eb6600e2635eefdf2e3
Cloter G. Barbosa Jr <cloter.junior@luizalabs.com>
2020-12-24 09:00:22 -0300

    chore: ignore protocols files from coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,16 @@ module.exports = {
   roots: ['<rootDir>/src'],
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'test-config',
+    'interfaces',
+    'jestGlobalMocks.ts',
+    '.module.ts',
+    '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '<rootDir>/src/presentation/protocols/index.ts',
+    '.mock.ts'
+  ],
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
   // The test environment that will be used for testing
