23bb05d3fa7d7cb395bbcc85480e933c2075c935
Cloter G. Barbosa Jr <cloter.junior@luizalabs.com>
2020-12-30 08:19:44 -0300

    refactor: make server error return the error stack

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,9 @@
 export class ServerError extends Error {
-  constructor () {
+  constructor (stack: string | undefined) {
     super('Internal server error')
     this.name = 'ServerError'
+    if (stack) {
+      this.stack = stack
+    }
   }
 }
