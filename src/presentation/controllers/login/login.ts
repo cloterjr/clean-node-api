7ebd9d409e19a51e57093dcd1ce324ae311fb459
Cloter G. Barbosa Jr <cloter.junior@luizalabs.com>
2020-12-31 11:00:26 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -11,15 +11,15 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse | undefined> {
-    if (!httpRequest.body.email) {
+    const { email, password } = httpRequest.body
+
+    if (!email) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
     }
 
-    if (!httpRequest.body.password) {
+    if (!password) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
     }
-
-    const { email } = httpRequest.body
 
     const isValid = this.emailValidator.isValid(email)
 
