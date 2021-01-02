e6767172aa674b961b181a22c20a6691734df81d
Cloter G. Barbosa Jr <cloter.junior@luizalabs.com>
2021-01-02 09:33:35 -0300

    refactor: move required field validation to the ValidationComposite

---
+++ src/main/factories/signup-validation.ts
@@ -0,0 +1,12 @@
+import { Validation } from '../../presentation/controllers/signup/signup-protocols'
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  return new ValidationComposite(validations)
+}
