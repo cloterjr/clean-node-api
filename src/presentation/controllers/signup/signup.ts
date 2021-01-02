648c021c687e56d1109e61f4d2053fb12e4b9bcb
Cloter G. Barbosa Jr <cloter.junior@luizalabs.com>
2021-01-02 08:59:30 -0300

    refactor: remove unused tests

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount, Validation } from './signup-protocols'
-import { MissingParamError, InvalidParamError } from '../../errors'
+import { InvalidParamError } from '../../errors'
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
@@ -19,14 +19,6 @@ export class SignUpController implements Controller {
       if (error) {
         return badRequest(error)
       }
-
-      const requiredFileds = ['name', 'email', 'password', 'passwordConfirmation']
-      for (const field of requiredFileds) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
-      }
-
       const { name, email, password, passwordConfirmation } = httpRequest.body
 
       if (password !== passwordConfirmation) {
