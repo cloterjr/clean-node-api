85a9491792ac34dc020761810eb1492ea010c415
Cloter G. Barbosa Jr <cloter.junior@luizalabs.com>
2021-03-28 08:48:27 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,6 @@
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
+export * from '../../../domain/usecases/authentication'
+export * from '../../protocols/db/account/load-account-by-email-repository'
 export * from '../../protocols/criptography/hasher'
 export * from '../../protocols/db/account/add-account-repository'
