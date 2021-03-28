105bb5c80ab85c3f554d905ff41870a99c51d621
Cloter G. Barbosa Jr <cloter.junior@luizalabs.com>
2021-03-28 08:54:15 -0300

    feat: ensure DBAddAccount returns null if LoadAccountByEmailRepository returns an account

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -9,9 +9,13 @@ export class DbAddAccount implements AddAccount {
   ) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel | null> {
-    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
-    const hashedPassword = await this.hasher.hash(accountData.password)
-    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
-    return new Promise(resolve => resolve(account))
+    const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password)
+      const newAccount = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+      return new Promise(resolve => resolve(newAccount))
+    }
+
+    return null
   }
 }
