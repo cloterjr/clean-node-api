645d0ca9f5bc9ceb1c4fbe8792b98dc6e45982ce
Cloter G. Barbosa Jr <cloter.junior@luizalabs.com>
2021-04-02 07:00:07 -0300

    feat: ensure AuthMiddleware returns 403 if no headers are found

--- src/presentation/protocols/middleware.ts
+++ src/presentation/protocols/middleware.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse } from './http'
 
-export interface Controller {
+export interface Middleware {
   handle: (httpRequest: HttpRequest) => Promise<HttpResponse | undefined>
 }
