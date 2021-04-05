d84819994bbdc5480eef20fc3f2bbe71c2a2218c
Cloter G. Barbosa Jr <cloter.junior@luizalabs.com>
2021-04-05 08:42:17 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,16 @@
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { JwtAdapter } from '../../infra/criptography/jwt-adapter/jwt-adapter'
 import app from '../config/app'
+import env from '../config/env'
 
-let surceyCollection: Collection
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const jwtAdapter = new JwtAdapter(env.jwtSecret)
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -15,8 +22,10 @@ describe('Survey Routes', () => {
   })
 
   beforeEach(async () => {
-    surceyCollection = await MongoHelper.getCollection('accounts')
-    await surceyCollection.deleteMany({})
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -34,6 +43,42 @@ describe('Survey Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid access token', async () => {
+      const password = await hash('123', 12)
+      const res = await accountCollection.insertOne({
+        name: 'Rodrigo',
+        email: 'rodrigo.maguinho@gmail.com',
+        password,
+        role: 'admin'
+      })
+
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      const encryptedAccessToken = await jwtAdapter.decrypt(accessToken)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken: encryptedAccessToken
+        }
+      })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
     })
   })
 })
